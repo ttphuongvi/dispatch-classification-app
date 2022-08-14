9880d132d9dc8fc9f441c3a5a3f8110a3c769edf
huytht <huytht2001@gmail.com>
2022-08-15 01:27:33 +0700

    update route for api list documentary

--- back-end/routes/appRoutes.js
+++ back-end/routes/appRoutes.js
@@ -3,10 +3,11 @@ module.exports = function (app) {
   var documentary = require("../controllers/documentary-controller");
   var category = require("../controllers/category-controller");
   // documentary Routes
-  app.route("/documentary").post(documentary.create_a_documentary);
+  app.route("/documentary")
+      .post(documentary.create_a_documentary)
+      .get(documentary.list_all_documentary);
   app.route("/documentary/:documentaryId").get(documentary.read_a_documentary);
   app.route("/documentary/image/:documentaryId").get(documentary.read_image_documentary);
-  app.route("/documentary/list").get(documentary.list_all_documentary);
   app.route("/documentary/list/:categoryId").get(category.list_all_documentary_by_category);
 
   // category Routes
